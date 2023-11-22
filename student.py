246aa7bd46a3c8232619b79380ad55f1c08c0da8
Arin Bagul <73395201+ArinBagul@users.noreply.github.com>
2023-11-22 17:12:14 +0530

    Update student.py

--- student.py
+++ student.py
@@ -18,10 +18,10 @@ class Student:
         else:
             try:
                 conn = mysql.connector.connect(
-                    host="localhost",
-                    username="root",
-                    password="1234",
-                    database="face_recognizer",
+                    host="your_host_name",
+                    username="your_mysql_username",
+                    password="your_password",
+                    database="your_database_name",
                 )
                 my_cursor = conn.cursor()
                 my_cursor.execute(
